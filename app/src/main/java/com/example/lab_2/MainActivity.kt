f4b523e70819e6311be26a15c7dc4f53d0b78103
strushkov <strushkov@scrile.com>
2024-10-19 14:30:36 +0700

    Добавил основную функциональность приложени Есть рабочий список и основные компоненты

--- app/src/main/java/com/example/lab_2/MainActivity.kt
+++ app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.lab_2
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.lab_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val adapter = TrackAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 1)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener{
+                var track = Track(R.drawable.forest, "Title1")
+                adapter.addTrack(track)
+            }
         }
     }
+
 }
